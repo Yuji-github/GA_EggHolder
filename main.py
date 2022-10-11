37f6ae1ce4d1047d93eb9eb2f82094789bd94b56
Ayami <a.nonaka.116@gmail.com>
2022-10-12 10:27:23 +1100

    changed the numpy to jaxnumpy

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+import numpy as np
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def f(x1,x2):
+    a=jnp.sqrt(jnp.fabs(x2+x1/2+47))
+    b=jnp.sqrt(jnp.fabs(x1-(x2+47)))
+    c=-(x2+47)*jnp.sin(a)-x1*jnp.sin(b)
+    return c
+x1=jnp.linspace(-512,512,100)
+x2=jnp.linspace(-512,512,100)
+X1,X2=jnp.meshgrid(x1,x2)
 
+def plotter():
+    fig=plt.figure(figsize=[12,8])
+    ax=plt.axes(projection='3d')
+    ax.plot_surface(X1,X2,f(X1,X2),color='red',alpha=0.7)
+    ax.plot_wireframe(X1,X2,f(X1,X2),ccount=2,rcount=2, color='orange',alpha=0.8)
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    ax.set_zlabel('f(x1,x2)')
+    plt.show()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi There. How are you? hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+plotter()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# if __name__ == '__main__':
+#     create_egg_holder()
